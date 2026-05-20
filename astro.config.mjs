9150da60bc49ac64afd4a1c78401204e7e099633
Xinghao Huang <xinghaohuang3@gmail.com>
2026-05-20 15:45:15 -0700

    remove sitemap integration for github pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://xhuang512.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), tailwind()],
 });
